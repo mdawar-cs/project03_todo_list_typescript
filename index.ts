63353678f96a15ff3e4198e80ae0e75bf1ec4913
Muhammad Dawar <72949307+mdawar-cs@users.noreply.github.com>
2023-01-01 16:22:41 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -31,6 +31,7 @@ async function addTask () {
 }
 
 async function addTasks() {
+  printTasks(tasks)
   do {
     await addTask();
     var again = await inquirer.prompt([
@@ -40,10 +41,12 @@ async function addTasks() {
             message: "Do you want to continue adding new Task to list Press y or Y",
         },
     ]);
-} while (again.restart == "y" || again.restart == "Y");
+  } while (again.restart == "y" || again.restart == "Y");
+  printTasks(tasks)
 }
 
 async function completeTask() {
+  printTasks(tasks)
   const userInput = await inquirer.prompt([
     {
       name: "taskID",
@@ -62,6 +65,7 @@ async function completeTask() {
 }
 
 async function deleteTask() {
+  printTasks(tasks)
   const userInput = await inquirer.prompt([
     {
       name: "taskID",
@@ -121,7 +125,22 @@ async function toDoApp() {
   }
 }
 
-await toDoApp();
+// await toDoApp();
+
+async function runAgain() {
+  do {
+      await toDoApp();
+      var again = await inquirer.prompt([
+          {
+              type: "input",
+              name: "restart",
+              message: "Do you want to continue use todo app? Press y or Y",
+          },
+      ]);
+  } while (again.restart == "y" || again.restart == "Y");
+}
+await runAgain();
+
 
 // do {
 //   await addTask();
